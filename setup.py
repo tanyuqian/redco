672e1878075733031083f12ba35a35b063a7fccc
Bowen <btan2@andrew.cmu.edu>
2022-12-21 21:59:12 -0500

    updated.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="redco",
+    version="0.0.1",
+    author="Bowen Tan",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    python_requires=">=3.8"
+)
