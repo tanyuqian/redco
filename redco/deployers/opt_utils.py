2f5a0268dcbd40759ccb17b1b44cf4c185bf1ebc
Bowen <btan2@andrew.cmu.edu>
2022-12-24 17:35:36 -0500

    updated.

--- redco/deployers/opt_utils.py
+++ redco/deployers/opt_utils.py
@@ -25,4 +25,4 @@ def get_multistep_adamw_optimizer(train_size,
         optax.adamw(learning_rate=lr_schedule_fn, weight_decay=weight_decay),
         every_k_schedule=accumulate_grad_batches)
 
-    return optimizer, lr_schedule_fn
+    return optimizer
