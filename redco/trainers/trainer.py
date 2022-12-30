154dedf2cff94f28cee84d7af9a78179f0ba62b2
Bowen <btan2@andrew.cmu.edu>
2022-12-30 13:36:27 -0500

    updated.

--- redco/trainers/trainer.py
+++ redco/trainers/trainer.py
@@ -100,14 +100,12 @@ class Trainer:
                 desc=f'epoch {epoch_idx}')
 
             if eval_fn is None:
-                eval_result = self.eval_loss(
+                loss = self.eval_loss(
                     examples=eval_examples,
                     per_device_batch_size=per_device_batch_size)
+                print(f'Epoch {epoch_idx}, evaluation loss = {loss}')
             else:
-                eval_result = eval_fn(params=self.params)
-
-            print(f'Epoch {epoch_idx}, Eval Result:')
-            print(json.dumps(eval_result, indent=4))
+                eval_fn()
 
     @property
     def params(self):
