dac82aecbfb4196da295baab063a7e09ade79ed5
Bowen <btan2@andrew.cmu.edu>
2023-01-06 14:40:15 -0500

    updated.

--- redco/utils/text_to_image_utils.py
+++ redco/utils/text_to_image_utils.py
@@ -6,7 +6,7 @@ import jax.numpy as jnp
 def preprocess(image, dtype):
     w, h = image.size
     w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
-    image = image.resize((w, h), resample=PIL_INTERPOLATION["lanczos"])
+    image = image.resize((w, h), resample=PIL.Image.LANCZOS)
     image = jnp.array(image).astype(dtype) / 255.0
     image = image[None].transpose(0, 3, 1, 2)
     return 2.0 * image - 1.0
