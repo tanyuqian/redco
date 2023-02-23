a75177c1d41aa6ce225660cb86dea35c46a63344
Bowen <btan2@andrew.cmu.edu>
2023-02-23 11:47:03 -0500

    updated.

--- redco/trainers/__init__.py
+++ redco/trainers/__init__.py
@@ -1,5 +1,4 @@
 from .trainer import Trainer
 from .image_to_text_trainer import ImageToTextTrainer
-from .text_to_text_trainer import TextToTextTrainer
 from .text_to_image_trainer import TextToImageTrainer
 from .maml_trainer import MAMLTrainer
