7bda09e2110d39401cbc82439f4974f047f8476d
Bowen Tan <btan2@andrew.cmu.edu>
2023-05-21 22:51:32 -0400

    updated.

--- examples/text_to_text/main.py
+++ examples/text_to_text/main.py
@@ -14,14 +14,92 @@
 
 from functools import partial
 import fire
-
+import numpy as np
 import jax
+import jax.numpy as jnp
+import optax
 import datasets
 from transformers import AutoTokenizer, FlaxAutoModelForSeq2SeqLM
-
+import evaluate
 from redco import Deployer, Trainer
-from text_to_text_pipeline import \
-    collate_fn, loss_fn, pred_fn, output_fn, eval_rouge
+
+
+def collate_fn(examples,
+               tokenizer,
+               decoder_start_token_id,
+               max_src_len,
+               max_tgt_len,
+               src_key='src',
+               tgt_key='tgt'):
+    model_inputs = tokenizer(
+        [example[src_key] for example in examples],
+        max_length=max_src_len,
+        padding='max_length',
+        truncation=True,
+        return_tensors='np')
+
+    decoder_inputs = tokenizer(
+        [example[tgt_key] for example in examples],
+        max_length=max_tgt_len,
+        padding='max_length',
+        truncation=True,
+        return_tensors='np')
+
+    if tokenizer.bos_token_id is not None:
+        labels = np.zeros_like(decoder_inputs['input_ids'])
+        labels[:, :-1] = decoder_inputs['input_ids'][:, 1:]
+        decoder_input_ids = decoder_inputs['input_ids']
+        decoder_input_ids[:, 0] = decoder_start_token_id
+    else:
+        labels = decoder_inputs['input_ids']
+        decoder_input_ids = np.zeros_like(decoder_inputs['input_ids'])
+        decoder_input_ids[:, 1:] = decoder_inputs['input_ids'][:, :-1]
+        decoder_input_ids[:, 0] = decoder_start_token_id
+
+    model_inputs['labels'] = labels
+    decoder_inputs['input_ids'] = decoder_input_ids
+
+    for key in decoder_inputs:
+        model_inputs[f'decoder_{key}'] = np.array(decoder_inputs[key])
+
+    return model_inputs
+
+
+def loss_fn(train_rng, state, params, batch, is_training):
+    labels = batch.pop("labels")
+    label_weights = batch['decoder_attention_mask']
+
+    logits = state.apply_fn(
+        **batch, params=params, dropout_rng=train_rng, train=is_training)[0]
+
+    loss = optax.softmax_cross_entropy_with_integer_labels(
+        logits=logits, labels=labels)
+
+    return jnp.sum(loss * label_weights) / jnp.sum(label_weights)
+
+
+def pred_fn(pred_rng, batch, params, model, gen_kwargs):
+    output_ids = model.generate(
+        input_ids=batch['input_ids'],
+        attention_mask=batch['attention_mask'],
+        params=params,
+        prng_key=pred_rng,
+        **gen_kwargs)
+    return output_ids.sequences
+
+
+def output_fn(batch_preds, tokenizer):
+    return tokenizer.batch_decode(batch_preds, skip_special_tokens=True)
+
+
+def eval_rouge(examples, preds, tgt_key):
+    rouge_scorer = evaluate.load('rouge')
+
+    return rouge_scorer.compute(
+        predictions=preds,
+        references=[example[tgt_key] for example in examples],
+        rouge_types=['rouge1', 'rouge2', 'rougeL'],
+        use_stemmer=True)
 
 
 def main(dataset_name='xsum',
@@ -45,13 +123,13 @@ def main(dataset_name='xsum',
     dataset = datasets.load_dataset(dataset_name)
     dataset = {key: list(dataset[key]) for key in dataset.keys()}
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     with jax.default_device(jax.devices('cpu')[0]):
         model = FlaxAutoModelForSeq2SeqLM.from_pretrained(
             model_name_or_path, from_pt=True)
         model.params = model.to_fp32(model.params)
 
-    gen_kwargs = {'max_length': max_tgt_len, 'num_beams': num_beams}
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        gen_kwargs = {'max_length': max_tgt_len, 'num_beams': num_beams}
 
     deployer = Deployer(
         jax_seed=jax_seed,
