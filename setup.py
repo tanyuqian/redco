fa048ebceee2b3b3b4deb85a06b028d56880a69b
Bowen Tan <btan2@andrew.cmu.edu>
2023-06-05 09:16:54 -0400

    updated.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="redco",
-    version="0.4.2",
+    version="0.4.4",
     author="Bowen Tan",
     packages=find_packages(),
     install_requires=['jax', 'flax', 'optax', 'numpy'],
