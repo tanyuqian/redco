c044e9e8857e0d3e2e2296931b66af3feac01255
Bowen Tan <btan2@andrew.cmu.edu>
2024-06-19 08:45:21 -0700

    updated.

--- examples/text_to_image/make_init_ckpt.py
+++ examples/text_to_image/make_init_ckpt.py
@@ -38,7 +38,7 @@ def main(model_name_or_path='stabilityai/stable-diffusion-2-1-base'):
     deployer = Deployer(workdir=None, jax_seed=0)
 
     ckpt_dir = './' + model_name_or_path.split('/')[-1]
-    print(f'ckpt_dir: {ckpt_dir}')
+    deployer.log_info(ckpt_dir, title='Init CKPT Dir to Save')
 
     with jax.default_device(jax.local_devices(backend='cpu')[0]):
         text_encoder = FlaxCLIPTextModel.from_pretrained(
