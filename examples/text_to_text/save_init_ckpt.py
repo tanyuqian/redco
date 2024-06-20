782b631f11c8c8e078f74202cb282b6200e7b456
Bowen Tan <btan2@andrew.cmu.edu>
2024-06-19 23:38:43 -0700

    updated.

--- examples/text_to_text/save_init_ckpt.py
+++ examples/text_to_text/save_init_ckpt.py
@@ -18,14 +18,14 @@ from transformers import FlaxAutoModelForSeq2SeqLM
 from redco import Deployer
 
 
-def main(model_name_or_path='huggyllama/llama-13b'):
-    deployer = Deployer(workdir=None, jax_seed=0)
+def main(model_name_or_path='google/flan-t5-xl'):
+    deployer = Deployer(workdir=None, jax_seed=42)
 
     ckpt_dir = './' + model_name_or_path.split('/')[-1]
     deployer.log_info(ckpt_dir, title='Init CKPT Dir to Save')
 
     with jax.default_device(jax.local_devices(backend='cpu')[0]):
-        model = FlaxAutoModelForCausalLM.from_pretrained(
+        model = FlaxAutoModelForSeq2SeqLM.from_pretrained(
             model_name_or_path, from_pt=True)
         deployer.save_ckpt(ckpt_dir=ckpt_dir, params=model.params)
 
